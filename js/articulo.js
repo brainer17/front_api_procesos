1f6c9dd2724eaa7c066fd86517edb2ed4cdcbb9f
brainer17 <80832574+brainer17@users.noreply.github.com>
2022-12-15 01:00:29 -0500

    Ultimos ajustes articulo js

--- js/articulo.js
+++ js/articulo.js
@@ -1,14 +1,8 @@
-//Todo de Articulo
-/***************************************************************************************************************
- * 
- * 
- * 
- * 
- * **************************************************************************************************************/
 
-
+/////////////////////////////////////Todo de Articulo///////////////////////////////////////////////////////////
 const urlApi2 = "http://localhost:9000";//colocar la url con el puerto
 
+//Listar los articulos 
 function listarArticulos(){
     validaToken();
     
@@ -26,10 +20,10 @@ function listarArticulos(){
         
             var articulos = `
             <div class="p-3 mb-2 bg-light text-dark">
-                    <h1 class="display-5"><i class="fa-solid fa-gift"></i> Listado de Articulos</h1>
+                    <h1 class="display-5"><i class="fa-solid fa-cart-shopping"></i> Listado de Articulos</h1>
                 </div>
                   
-                <a href="#" onclick="registerFormArticulo('true')" class="btn btn-outline-success"><i class="fa-solid fa-user-plus"></i></a>
+                <a href="#" onclick="registerFormArticulo('true')" class="btn btn-outline-success"><i class="fa-sharp fa-solid fa-cart-plus"></i></a>
                 <table class="table">
                     <thead>
                     <tr>
@@ -39,9 +33,10 @@ function listarArticulos(){
                     <th scope="col">Descripcion</th>
                     <th scope="col">Fecha de registro</th>
                     <th scope="col">Stock</th>
-                    <th scope="col">Categoria</th>     
+                    <th scope="col">Categoria</th>    
+                    <th scope="col">Precio de compra</th> 
                     <th scope="col">Precio de venta</th>
-                    <th scope="col">Precio de compra</th>
+                   
                     <th scope="col">Action</th>
                     </tr>
                     </thead>
@@ -59,15 +54,17 @@ function listarArticulos(){
                             <td>${articulo.fecha_registro}</td>
                             <td>${articulo.stock}</td>
                             <th>${articulo.categoria.nombre}</th>
-                            <td>${articulo.precio_venta}</td>
                             <td>${articulo.precio_compra}</td>
+                            <td>${articulo.precio_venta}</td>
+                            
                             <td>
                             <button type="button" class="btn btn-outline-danger" 
                             onclick="eliminaArticulo('${articulo.codigo}')">
-                                <i class="fa-solid fa-user-minus"></i>
+                                <i class="fa-solid fa-trash"></i>
                             </button>
                             <a href="#" onclick="verModificarArticulo('${articulo.codigo}')" class="btn btn-outline-warning">
-                                <i class="fa-solid fa-user-pen"></i>
+                                <i class="fa-solid fa-pen-to-square"></i>
+
                             </a>
                             <a href="#" onclick="verArticulo('${articulo.codigo}')" class="btn btn-outline-info">
                                 <i class="fa-solid fa-eye"></i>
@@ -85,7 +82,7 @@ function listarArticulos(){
           })
 }
 
-
+// Ver cada alrticulo 
 function verArticulo(codigo){
     validaToken();
     var settings={
@@ -112,13 +109,14 @@ function verArticulo(codigo){
                     <li class="list-group-item">Fecha de Registro: ${articulo.fecha_registro}</li>
                     <li class="list-group-item">Stock: ${articulo.stock}</li>
                     <li class="list-group-item">Categoria: ${articulo.categoria.nombre}</li>
-                    <li class="list-group-item">Usuario: ${articulo.usuario.nombre}</li>
-                    <li class="list-group-item">Precio de venta: ${articulo.precio_venta}</li>
                     <li class="list-group-item">Precio de compra: ${articulo.precio_compra}</li>
+                    <li class="list-group-item">Precio de venta: ${articulo.precio_venta}</li>
+                    
                 </ul>`;
               
             }
             document.getElementById("contentModal").innerHTML = cadena;
+            document.getElementById("exampleModalLabel").innerHTML = "Gestión de Articulos";
             var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
             myModal.toggle();
     })
@@ -129,7 +127,7 @@ function verArticulo(codigo){
 function registerFormArticulo(auth=false){
     cadena = `
             <div class="p-3 mb-2 bg-light text-dark">
-                <h1 class="display-5"><i class="fa-solid fa-gift"></i> Registrar Articulo</h1>
+                <h1 class="display-5"><i class="fa-solid fa-cart-shopping"></i> Registrar Articulo</h1>
             </div>
               
             <form action="" method="post" id="myFormReg1">
@@ -143,14 +141,13 @@ function registerFormArticulo(auth=false){
             <label for="fecha_registro"  class="form-label">Fecha de registro</label>
             <input type="date" class="form-control" name="fecha_registro" id="fecha_registro" > <br>
             <label for="stock"  class="form-label">Stock</label>
-            <input type="number" class="form-control" name="stock" id="stock" > <br>
-            <br>
+            <input type="number" class="form-control" name="stock" id="stock" ><br>
             <label for="categoria" class="form-label">Categoria</label>
             <input type="number" class="form-control" id="categoria" name="categoria" required> <br>
-            <label for="precio_venta"  class="form-label">Precio de venta</label>
-            <input type="number" class="form-control" name="precio_venta" id="precio_venta" > <br>
             <label for="precio_compra" class="form-label">precio de compra</label>
             <input type="number" class="form-control" name="precio_compra" id="precio_compra" required> <br>
+            <label for="precio_venta"  class="form-label">Precio de venta</label>
+            <input type="number" class="form-control" name="precio_venta" id="precio_venta" > <br>
                 <button type="button" class="btn btn-outline-info" onclick="registrarArticulo('${auth}')">Registrar</button>
             </form>`;
             document.getElementById("contentModal").innerHTML = cadena;
@@ -158,8 +155,6 @@ function registerFormArticulo(auth=false){
             var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
             myModal.toggle();
 }
-
-
 
 async function registrarArticulo(auth=false){
     validaToken();
@@ -202,27 +197,120 @@ async function registrarArticulo(auth=false){
 
 
 
+// Modificar articulo
+function verModificarArticulo(codigo){
+    validaToken();
+    var settings={
+        method: 'GET',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+    }
+    fetch(urlApi2+"/articulo/codigo/"+codigo,settings)
+    .then(response => response.json())
+    .then(function(articulo){
+            var cadena='';
+            if(articulo){                
+                cadena = `
+                <div class="p-3 mb-2 bg-light text-dark">
+                    <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Modificar Articulo</h1>
+                </div>
+              
+                <form action="" method="post" id="myForm1">
+                    <input type="hidden" name="id" id="id" value="${articulo.id}">
+                    <label for="codigo"  class="form-label">codigo</label>
+                    <input type="text" class="form-control" name="codigo" id="codigo" value="${articulo.codigo}" required> <br>
+                    <label for="descripcion"  class="form-label">Descripcion</label>
+                    <label for="nombre" class="form-label">Nombre</label>
+                    <input type="text" class="form-control" name="nombre" id="nombre" value="${articulo.nombre}" required> <br>
+                    <input type="text" class="form-control" name="descripcion" id="descripcion" value="${articulo.descripcion}" required> <br>
+                    <label for="categoria" class="form-label">Categoria</label>
+                    <input type="text" class="form-control" id="categoria" name="categoria" value="${articulo.categoria.id}" required> <br>
+                    <label for="stock" class="form-label">Stock</label>
+                    <input type="number" class="form-control" id="stock" name="stock" value="${articulo.stock}" required> <br>
+                    <label for="precio_compra" class="form-label">Precio de compra</label>
+                    <input type="number" class="form-control" id="precio_compra" name="precio_compra" value="${articulo.precio_compra}" required> <br>
+                    <label for="precio_venta" class="form-label">Precio de venta</label>
+                    <input type="number" class="form-control" id="precio_venta" name="precio_venta" value="${articulo.precio_venta}" required> <br>
+                    <button type="button" class="btn btn-outline-warning" 
+                        onclick="modificarArticulo('${articulo.codigo}')">Modificar
+                    </button>
+                </form>`;
+            }
+            document.getElementById("contentModal").innerHTML = cadena;
+            document.getElementById("exampleModalLabel").innerHTML = "Gestión de Articulos";
+            var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
+            myModal.toggle();
+    })
+}
+async function modificarArticulo(codigo){
+    validaToken();
+    var myForm = document.getElementById("myForm1");
+    var formData = new FormData(myForm);
+    var jsonData = {};
+    for(var [k, v] of formData){//convertimos los datos a json
+        jsonData[k] = v;
+    }
+    const request = await fetch(urlApi2+"/articulo/codigo/"+codigo, {
+        method: 'PUT',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+        body: JSON.stringify(jsonData)
+    });
+    listarArticulos();
+    alertas("Se ha modificado el articulo exitosamente!",1)
+    document.getElementById("contentModal").innerHTML = '';
+    var myModalEl = document.getElementById('modalUsuario')
+    var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
+    modal.hide();
+}
 
 
 
 
 
+// Eliminar Articulo
+function eliminaArticulo(codigo){
+    validaToken();
+    var settings={
+        method: 'DELETE',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+    }
+    fetch(urlApi2+"/articulo/"+codigo,settings)
+    .then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+        listarArticulos();
+        alertas("Se ha eliminado el Articulo exitosamente!",2)
+      })
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+function alertas(mensaje,tipo){
+    var color ="warning";
+    if(tipo == 1){//success verde
+        color="success"
+    }
+    else{//danger rojo
+        color = "danger"
+    }
+    var alerta =`<div class="alert alert-${color} alert-dismissible fade show" role="alert">
+                    <strong><i class="fa-solid fa-triangle-exclamation"></i></strong>
+                        ${mensaje}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                 </div>`;
+    document.getElementById("alerta").innerHTML = alerta;
+}
+//salir de la session
 function salir(){
     localStorage.clear();
     location.href = "index.html";
