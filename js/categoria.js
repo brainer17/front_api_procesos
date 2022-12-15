c7b8edd66c0e9aca592b00026dc817b239af6f79
brainer17 <80832574+brainer17@users.noreply.github.com>
2022-12-15 00:59:59 -0500

    Categoria listo

--- js/categoria.js
+++ js/categoria.js
@@ -1,7 +1,7 @@
-//Todo de Categoria//
+//////////////////////Todo de Categoria/////////////////////////////////////
 const urlApi3 = "http://localhost:9000";//colocar la url con el puerto
 
-
+//Listar categoria
 function listarCategorias(){
     validaToken();
     
@@ -22,7 +22,7 @@ function listarCategorias(){
                     <h1 class="display-5"><i class="fa-solid fa-list"></i> Listado de Categoria</h1>
                 </div>
                   
-                <a href="#" onclick="registerFormCategoria('true')" class="btn btn-outline-success"><i class="fa-solid fa-user-plus"></i></a>
+                <a href="#" onclick="registerFormCategoria('true')" class="btn btn-outline-success"><i class="fa-solid fa-list"></i></a>
                 <table class="table">
                     <thead>
                         <tr>
@@ -46,11 +46,12 @@ function listarCategorias(){
                         <td>
                         <button type="button" class="btn btn-outline-danger" 
                         onclick="eliminaCategoria('${categoria.id}')">
-                            <i class="fa-solid fa-user-minus"></i>
+                            <i class="fa-solid fa-trash"></i>
                         </button>
                         
                         <a href="#" onclick="verModificarCategoria('${categoria.id}')" class="btn btn-outline-warning">
-                            <i class="fa-solid fa-user-pen"></i>
+                            <i class="fa-solid fa-pen-to-square"></i>
+
                         </a>
                         <a href="#" onclick="verCategoria('${categoria.id}')" class="btn btn-outline-info">
                             <i class="fa-solid fa-eye"></i>
@@ -68,7 +69,7 @@ function listarCategorias(){
             
     })
 }
-
+// Ver cada categoria
 function verCategoria(id){
     validaToken();
     var settings={
@@ -86,50 +87,38 @@ function verCategoria(id){
             if(categoria){                
                 cadena = `
                 <div class="p-3 mb-2 bg-light text-dark">
-                    <h1 class="display-5"><i class="fa-solid fa-user-pen"></i>Visualizar Categoria</h1>
+                    <h1 class="display-5"><i class="fa-solid fa-list"></i> Visualizar Categoria</h1>
                 </div>
                 <ul class="list-group">
                     <li class="list-group-item">Nombre: ${categoria.nombre}</li>
-                    <li class="list-group-item">Descripcion: ${categoria.descripcion}</li>
+                    <li class="list-group-item">Descripción: ${categoria.descripcion}</li>
                 </ul>`;
               
             }
             document.getElementById("contentModal").innerHTML = cadena;
+            document.getElementById("exampleModalLabel").innerHTML = "Gestión de Categoria";
             var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
             myModal.toggle();
     })
 }
-function alertas(mensaje,tipo){
-    var color ="";
-    if(tipo == 1){//success verde
-        color="success"
-    }
-    else{//danger rojo
-        color = "danger"
-    }
-    var alerta =`<div class="alert alert-'+color+' alert-dismissible fade show" role="alert">
-                    <strong><i class="fa-solid fa-triangle-exclamation"></i></strong>
-                        ${mensaje}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                 </div>`;
-    document.getElementById("alerta").innerHTML = alerta;
-}
+
 // Formularo de categoria
 function registerFormCategoria(){
     cadena = `
             <div class="p-3 mb-2 bg-light text-dark">
-                <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Registrar Categoria</h1>
+                <h1 class="display-5"><i class="fa-solid fa-list"></i> Registrar Categoria</h1>
             </div>
               
             <form action="" method="post" id="myForm1">
                 <input type="hidden" name="id" id="id">
                 <label for="nombre" class="form-label">Nombre</label>
                 <input type="text" class="form-control" name="nombre" id="nombre" required> <br>
-                <label for="descripcion"  class="form-label">Descripcion</label>
+                <label for="descripcion"  class="form-label">Descripción</label>
                 <input type="text" class="form-control" name="descripcion" id="descripcion" required> <br>
                 <button type="button" class="btn btn-outline-info" onclick="registrarCategoria()">Registrar</button>
             </form>`;
             document.getElementById("contentModal").innerHTML = cadena;
+            document.getElementById("exampleModalLabel").innerHTML = "Gestión de Categoria";
             var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
             myModal.toggle();
 }
@@ -159,7 +148,7 @@ async function registrarCategoria(){
     var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
     modal.hide();
 }
-// Ver el Formulario 
+// Ver el Formulario de categoria 
 function verModificarCategoria(id){
     validaToken();
     var settings={
@@ -177,14 +166,14 @@ function verModificarCategoria(id){
             if(categoria){                
                 cadena = `
                 <div class="p-3 mb-2 bg-light text-dark">
-                    <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Modificar Categoria</h1>
+                    <h1 class="display-5"><i class="fa-solid fa-list"></i> Modificar Categoria</h1>
                 </div>
               
                 <form action="" method="post" id="myForm">
                     <input type="hidden" name="id" id="id" value="${categoria.id}">
                     <label for="nombre" class="form-label">Nombre</label>
                     <input type="text" class="form-control" name="nombre" id="nombre" required value="${categoria.nombre}"> <br>
-                    <label for="descripcion"  class="form-label">Descripcion</label>
+                    <label for="descripcion"  class="form-label">Descripción</label>
                     <input type="text" class="form-control" name="descripcion" id="descripcion" required value="${categoria.descripcion}"> <br>
                     <button type="button" class="btn btn-outline-warning" 
                         onclick="modificarCategoria('${categoria.id}')">Modificar
@@ -192,11 +181,14 @@ function verModificarCategoria(id){
                 </form>`;
             }
             document.getElementById("contentModal").innerHTML = cadena;
+            document.getElementById("exampleModalLabel").innerHTML = "Gestión de Categoria";
             var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
             myModal.toggle();
     })
 }
 
+
+//Modficar categoria
 async function modificarCategoria(id){
     validaToken();
     var myForm = document.getElementById("myForm");
@@ -223,21 +215,45 @@ async function modificarCategoria(id){
 }
 
 
+// Eliminar categoria 
+function eliminaCategoria(id){
+    validaToken();
+    var settings={
+        method: 'DELETE',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+    }
+    fetch(urlApi3+"/categoria/"+id,settings)
+    .then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+        listarCategorias();
+        alertas("Se ha eliminado el categoria exitosamente!",2)
+      })
+}
 
 
 
 
+function alertas(mensaje,tipo){
+    var color ="warning";
+    if(tipo == 1){//success verde
+        color="success"
+    }
+    else{//danger rojo
+        color = "danger"
+    }
+    var alerta =`<div class="alert alert-${color} alert-dismissible fade show" role="alert">
+                    <strong><i class="fa-solid fa-triangle-exclamation"></i></strong>
+                        ${mensaje}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                 </div>`;
+    document.getElementById("alerta").innerHTML = alerta;
+}
 
-
-
-
-
-
-
-
-
-
-
+//Salir de la session
 
 function salir(){
     localStorage.clear();
